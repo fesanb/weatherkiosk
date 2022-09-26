5a37a2c1231b772981d9e51451da5f05a5086cbe
StefanB <stefan@bahrawy.net>
2022-09-26 18:31:09 +0200

    Next step icons

--- weatherkiosk.py
+++ weatherkiosk.py
@@ -9,6 +9,7 @@ from tkinter import *
 from PIL import ImageTk, Image
 import os
 
+weather_list = 20
 
 
 url = 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=59.96&lon=10.74'
@@ -19,6 +20,50 @@ rdata = response.text
 jdata = json.loads(rdata)
 
 partlycloudy_night = "03n.png"
+
+symbols = [
+["clearsky", "01"],
+["cloudy", "04"],
+["fair", "02"],
+["fog", "15"],
+["heavyrain", "10"],
+["heavyrainandthunder", "11"],
+["heavyrainshowers", "41"],
+["heavyrainshowersandthunder", "25"],
+["heavysleet", "48"],
+["heavysleetandthunder", "32"],
+["heavysleetshowers", "43"],
+["heavysleetshowersandthunder", "27"],
+["heavysnow", "50"],
+["heavysnowandthunder", "34"],
+["heavysnowshowers", "45"],
+["heavysnowshowersandthunder", "29"],
+["lightrain", "46"],
+["lightrainandthunder", "30"],
+["lightrainshowers", "40"],
+["lightrainshowersandthunder", "24"],
+["lightsleet", "47"],
+["lightsleetandthunder", "31"],
+["lightsleetshowers", "42"],
+["lightsnow", "49"],
+["lightsnowandthunder", "33"],
+["lightsnowshowers", "44"],
+["lightssleetshowersandthunder", "26"],
+["lightssnowshowersandthunder", "28"],
+["partlycloudy", "03"],
+["rain", "09"],
+["rainandthunder", "22"],
+["rainshowers", "05"],
+["rainshowersandthunder", "06"],
+["sleet", "12"],
+["sleetandthunder", "23"],
+["sleetshowers", "07"],
+["sleetshowersandthunder", "20"],
+["snow", "13"],
+["snowandthunder", "14"],
+["snowshowers", "08"],
+["snowshowersandthunder", "21"]
+]
 
 
 def fetch_weather(wid):
@@ -32,6 +77,47 @@ def fetch_weather(wid):
     cleantime = time[11:16]
 
     return cleantime, temp, wind, rain, symbol
+
+
+
+
+
+def fetch_symbols():
+    isymb = 0
+    weather_code = []
+
+
+    while isymb < weather_list:
+        weather_code.append(str(jdata['properties']['timeseries'][isymb]['data']['next_1_hours']['summary']['symbol_code']))
+        isymb += 1
+
+    weather_code_red = [*set(weather_code)]
+    print(weather_code_red)
+
+    for list in symbols:
+        if weather_code_red[0] in list:
+            weather_symbol.append(weather_code_red[0], )
+
+
+        lists = [[1, 2, 3], [4, 5, 6]]
+
+        element = 4
+
+        element_in_lists = False
+
+        for list in lists:
+            if
+        element in list:
+
+        element_in_lists = True
+
+        print(element_in_lists)
+
+
+
+
+fetch_symbols()
+
 
 
 win = Tk() #creating the main window and storing the window object in 'win'
@@ -57,6 +143,7 @@ win.columnconfigure(2, minsize=100)
 win.columnconfigure(3, minsize=100)
 win.columnconfigure(4, minsize=100)
 
+
 def pick_img(code):
     if code == 18:
         img = "w-img/03n.png"
@@ -69,7 +156,7 @@ row = 1
 timeseri = 0
 i = 18
 
-while i < 20:
+while i < weather_list:
     wl0 = Label(win, text="kl " + fetch_weather(timeseri)[0])
     wl1 = Label(win, text=fetch_weather(timeseri)[1] + " °C")
     wl2 = Label(win, text=fetch_weather(timeseri)[2] + " m/s")
